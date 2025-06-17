d51b1d25c41641d4f43a879aee50232cce8e5f34
Juan Berrios <jberrioscifuentes@gmail.com>
2025-06-17 08:59:21 -0400

    Add SEO configuration + Add social example files

---
+++ src/@types/index.ts
@@ -0,0 +1,10 @@
+export type SEOProps = {
+  title?: string;
+  description?: string;
+  ogImage?: {
+    src: string;
+  };
+  copyrightText?: string;
+  twitterUsername?: string;
+  twitterDomain?: string;
+};
---
+++ src/config.ts
@@ -0,0 +1,10 @@
+import socialImage from "./assets/images/social.webp";
+
+export const SITE = {
+  title: "Default page title",
+  description: "A default page desc",
+  website: "https://www.domain.com",
+  author: "Name Surname",
+  profile: "https://www.author.com",
+  ogImage: socialImage.src,
+} as const;
