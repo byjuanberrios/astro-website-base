f9cc731579dd6b37634097ffc5c191e2e400f86e
Juan Berrios <jberrioscifuentes@gmail.com>
2025-06-19 15:34:46 -0400

    Add config to resolve alias on production for react-dom/server

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,16 @@ export default defineConfig({
   integrations: [react()],
   adapter: cloudflare(),
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias:
+        process.env.NODE_ENV === 'production'
+          ? {
+              'react-dom/server': 'react-dom/server.edge',
+            }
+          : undefined,
+    },
   }
 });
