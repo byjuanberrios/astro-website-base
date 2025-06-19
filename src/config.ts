456bc0939c23d6113fa18603e35d766245224a20
Juan Berrios <jberrioscifuentes@gmail.com>
2025-06-19 15:27:06 -0400

    Default Config refactor: include optional copyright, twitter username and twitter domain properties as global SEO parameters and update components to use new props

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,16 @@
 import socialImage from "./assets/images/social.webp";
 
-export const SITE = {
+export const SITE: {
+  name: string;
+  description: string;
+  website: string;
+  author: string;
+  profile: string;
+  ogImage: string;
+  copyright?: string;
+  twitterUsername?: string;
+  twitterDomain?: string;
+} = {
   name: "Nombre del sitio web",
   description:
     "Template hecho en Astro que sirve como bootstraper para mis proyectos",
@@ -8,4 +18,7 @@ export const SITE = {
   author: "Name Surname",
   profile: "https://www.author.com",
   ogImage: socialImage.src,
+  // copyright: "(c) Nombre de la empresa",
+  // twitterUsername: "@nombredeusuario",
+  // twitterDomain: "www.example.com",
 } as const;
